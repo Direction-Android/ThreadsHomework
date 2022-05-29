1feccdd569357a1a40d49bf3f3ab2614a5b771be
SalimovAzimjon <azim.salimov.99@gmail.com>
2022-05-29 22:28:53 +0500

    homework created

---
+++ app/src/main/java/uz/direction/threadshomework/MainActivity.kt
@@ -0,0 +1,18 @@
+package uz.direction.threadshomework
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import uz.direction.threadshomework.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+}
